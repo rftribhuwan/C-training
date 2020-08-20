65dcb4b6a1f7b7a492e83723357c60bdc36aeac9
Reuben.Tribhuwan <Reuben.Tribhuwan@IL013413>
2020-08-20 21:25:06 +0530

    Minimum number in Array Complete code

---
+++ Day2ArrayMinimum/ArrayMinimum/Minimum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMinimum
+{
+    class Minimum
+    {
+        public int FindMinimum(int[] array, int numberOfElements)
+        {
+            int i;
+            int smallestNumber = array[0];
+            for(i=1;i<numberOfElements;i++)
+            {
+                if (array[i] < smallestNumber)
+                    smallestNumber = array[i];
+            }
+            return smallestNumber;
+        }
+    }
+}
---
+++ Day2ArrayMinimum/ArrayMinimum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMinimum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[100];
+            int n, i, minimum;
+            Console.WriteLine("\nPlease enter the amount of numbers to use");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nPlease enter the numbers");
+            for (i=0;i<n;i++)
+            {
+                array1[i] = int.Parse(Console.ReadLine());
+            }
+            var object1 = new Minimum();
+            minimum = object1.FindMinimum(array1,n);
+            Console.WriteLine("Smallest Number in the array is:-\t"+minimum);
+        }
+    }
+}
