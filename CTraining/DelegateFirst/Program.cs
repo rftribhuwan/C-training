e182b5ed4f64f79a37f8699c9b5ce0b594efb153
Reuben.Tribhuwan <Reuben.Tribhuwan@IL013413>
2020-09-09 18:44:05 +0530

    Adding Delegate Codes

---
+++ CTraining/DelegateFirst/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateFirst
+{
+    class Program
+    {
+        public delegate int Addition(int firstNum, int secondNum);
+        public delegate int Subtraction(int firstNum, int secondNum);
+
+        public int Add(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+        public int Sub(int firstNum, int secondNum)
+        {
+            return firstNum - secondNum;
+        }
+        static void Main(string[] args)
+        {
+            int firstNum, secondNum;
+            Console.WriteLine("Please enter first number!");
+            firstNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter second number!");
+            secondNum = int.Parse(Console.ReadLine());
+            Program program = new Program();
+            Addition object1 = new Addition(program.Add);
+            Subtraction object2 = new Subtraction(program.Sub);
+            int result1 = object1(firstNum, secondNum);
+            int result2 = object2(firstNum, secondNum);
+            Console.WriteLine("Addition=" + result1);
+            Console.WriteLine("Subtraction=" + result2);
+
+        }
+    }
+}
---
+++ CTraining/DelegateSecond/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateSecond
+{
+    class Program
+    {
+        public delegate void Rectangle(float firstNum, float secondNum);
+        public void Area(float firstNum, float secondNum)
+        {
+            Console.WriteLine("Area of rectangle=" + (firstNum * secondNum));
+        }
+        public void Perimeter(float firstNum, float secondNum)
+        {
+            Console.WriteLine("Perimeter of rectangle=" + (2 * (firstNum + secondNum)));
+        }
+        static void Main(string[] args)
+        {
+            int firstNum, secondNum;
+            Console.WriteLine("Please enter first number!");
+            firstNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter second number!");
+            secondNum = int.Parse(Console.ReadLine());
+            Program program = new Program();
+            Rectangle rectangle = new Rectangle(program.Area);
+            rectangle += program.Perimeter;
+            rectangle(firstNum, secondNum);
+        }
+    }
+}
